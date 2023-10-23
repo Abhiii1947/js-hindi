cef3376962e5efe8e0347ef0a2f62a2b56dbf5d5
Abhiii1947 <116731373+Abhiii1947@users.noreply.github.com>
2023-10-23 11:21:39 +0000

    know about conversions and operations

---
+++ 01_basics/03_conversionAndOperation.js
@@ -0,0 +1,63 @@
+let score = "88abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not A Number)
+// true => 1; false => 0
+
+let isLoggedIn = "Abhishek"
+// console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhishek" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(Number)
+// console.log(typeof stringNumber);
+
+
+
+// ********* Operations ***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Abhishek"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32 
+
+// console.log(true); //true// console.log(+true); //1
+// console.log(true+); //error
+// console.log(+""); //0
+
+let gameCounter = 100
+// console.log(gameCounter++); //100
+// console.log(++gameCounter); //101
+
