92284e513e6d3f9e74734d6950cbd73b5931a9a1
Abhiii1947 <116731373+Abhiii1947@users.noreply.github.com>
2023-10-23 12:45:02 +0000

    change in location of files

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,20 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 1234567898765n
+
+const heros = ["shaktiman","ironman","hulk"];
+let myObj = {
+    name: "Abhishek",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+console.log(typeof heros);
