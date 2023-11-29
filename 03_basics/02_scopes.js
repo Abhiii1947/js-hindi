86d18c0210240269defa9856e7f9f41d074b17e4
Abhiii1947 <116731373+Abhiii1947@users.noreply.github.com>
2023-11-29 12:44:50 +0000

    scope and hoist

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,7 +5,20 @@ if(true){
     const b = 20
     var c = 30
 
-    console.log("Inner: ", a); // a = 10
+    // console.log("Inner: ", a); // a = 10
 }
 
-console.log(a);  // a = 300
+// console.log(a);  // a = 300
+
+
+function addOne(num1)  {
+    return num1 + 1
+}
+console.log(addOne(5));
+
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(5));
