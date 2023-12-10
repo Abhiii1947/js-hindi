eece729c0a7c31f25b76c892135a94d54ffd38e7
Abhiii1947 <116731373+Abhiii1947@users.noreply.github.com>
2023-12-10 11:50:56 +0000

    js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = 3
+
+switch (3) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("frbruary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
